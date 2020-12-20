98c5030434320bc86d982c369796bd864ba6804a
2036 <2036@users.noreply.github.com>
2020-12-20 18:40:53 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,13 +11,13 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Add a passwall depend	
-#git clone https://github.com/kenzok8/small.git package/small
+git clone https://github.com/kenzok8/small.git package/small
 
 # Add a passwall	
-#git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
